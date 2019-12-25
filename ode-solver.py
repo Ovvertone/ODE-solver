8a23e99d41f22d77e0d18a883dce9fd0410df851
Vyacheslav Shirkeev <shirkeev_v.v@mail.ru>
2019-12-25 22:59:32 +0700

    Added ability to enter negative integration limits and other minor updates

--- ode-solver.py
+++ ode-solver.py
@@ -10,26 +10,28 @@ print("{:<}".format("|"), "{:^54}".format("ay'''(t) + by''(t) + cy'(t) + dy(t) =
 print("{:<}".format("|"), "{:^55}".format("y(0) = y\u2080, y'(0) = y'\u2080, y''(0) = y''\u2080"), "{:>}".format("|"))
 print("_" * 58)
 
-LOWER_LIMIT = 0
-UPPER_LIMIT = int(input(Style.NORMAL + "\nВведите верхний предел интегрирования: "))
+LOWER_LIMIT = float(input(Style.NORMAL + "\nВведите нижний предел интегрирования: "))
+UPPER_LIMIT = float(input("Введите верхний предел интегрирования: "))
 
 while not UPPER_LIMIT > LOWER_LIMIT:
     try:
         raise ValueError
     except ValueError as error:
-        print(Fore.RED + "Верхний предел интегрирования должен быть больше 0")
-        UPPER_LIMIT = int(input(Style.NORMAL + "Введите верхний предел интегрирования: "))
+        print(Fore.RED + "Верхний предел интегрирования должен быть больше, чем нижний предел интегрирования")
+        UPPER_LIMIT = float(input(Style.NORMAL + "Введите верхний предел интегрирования: "))
+
+LIMIT_DIFF = abs(UPPER_LIMIT - LOWER_LIMIT)
 
 step = float(input(Style.NORMAL + "Введите шаг интегрирования: "))
 
-while not LOWER_LIMIT < step <= UPPER_LIMIT:
+while (not 0 < step <= LIMIT_DIFF):
     try:
         raise ValueError
-    except ValueError as error:
-        print(Fore.RED + f"Шаг интегрирования должен быть в интервале (0:{UPPER_LIMIT}]")
+    except ValueError:
+        print(Fore.RED + f"Шаг интегрирования должен быть в интервале (0:{LIMIT_DIFF}]")
         step = float(input(Style.NORMAL + "Введите шаг интегрирования: "))
 
-step_count =  UPPER_LIMIT // step
+step_count =  LIMIT_DIFF // step
 
 try:
     x_0 = float(input("Введите значение y(0): "))
@@ -56,8 +58,8 @@ except ValueError:
     exit(-1)
 
 sol_x, sol_y, sol_z = [x_0], [y_0], [z_0]
-t = [0]
-step_number = 0
+t = [LOWER_LIMIT]
+step_number = LOWER_LIMIT
 i = 0
 
 
@@ -65,27 +67,30 @@ def diff_equation(y, y1, y2, t):
     return 1 / (2*t+2) - y - y1 - y2
 
 
-print('\n{:<22}'.format("t"), '{:<22}'.format("y"), '{:<22}'.format("y'"), '{:<22}'.format("y''"))
-print('_' * 90)
-
-
 def Euler(step_count, i, t, step_number, sol_x, sol_y, sol_z):
-    while i < step_count:
-        step_number += step
-        i += 1
-        t.append(step_number)
-        sol_x.append(sol_x[i - 1] + step * sol_y[i-1])
-        sol_y.append(sol_y[i - 1] + step * sol_z[i-1])
-        sol_z.append(sol_z[i - 1] + step * diff_equation(sol_x[i - 1], sol_y[i - 1], sol_z[i - 1], t[i - 1]))
-        if i == step_count and step_number < UPPER_LIMIT:
-            t.append(float(UPPER_LIMIT))
+    try:
+        while i < step_count:
+            step_number += step
+            t.append(step_number)
             sol_x.append(sol_x[i] + step * sol_y[i])
             sol_y.append(sol_y[i] + step * sol_z[i])
             sol_z.append(sol_z[i] + step * diff_equation(sol_x[i], sol_y[i], sol_z[i], t[i]))
+            i += 1
+            if i == step_count and step_number < UPPER_LIMIT:
+                t.append(float(UPPER_LIMIT))
+                sol_x.append(sol_x[i] + step * sol_y[i])
+                sol_y.append(sol_y[i] + step * sol_z[i])
+                sol_z.append(sol_z[i] + step * diff_equation(sol_x[i], sol_y[i], sol_z[i], t[i]))
+    except ZeroDivisionError:
+        print(Fore.RED + "В процессе вычисления возникла угроза деления на ноль!")
+        exit(-1)
     return sol_x, sol_y, sol_z, t
 
 
 Euler(step_count, i, t, step_number, sol_x, sol_y, sol_z)
+
+print('\n{:<22}'.format("t"), '{:<22}'.format("y"), '{:<22}'.format("y'"), '{:<22}'.format("y''"))
+print('_' * 90)
 
 for line in range(len(t)):
     print('{:<22}'.format(t[line]),
